daee60763819eaf6a507bad53e773142d9583578
gomuloso <m4nuel4lf4ro@protonmail.com>
2023-08-09 12:03:58 +0200

    beauty changes

--- myfirstpokedex/src/App.jsx
+++ myfirstpokedex/src/App.jsx
@@ -20,6 +20,7 @@ function App() {
     searchPokemon(userSearch)
   }, [userSearch])
 
+  
 
   return (
     <>
--- myfirstpokedex/src/components/Display.jsx
+++ myfirstpokedex/src/components/Display.jsx
@@ -1,30 +1,25 @@
 import React, { useEffect } from 'react'
 
 export default function Display({ pokemon }) {
-
-  useEffect(() => {
-    console.log(`el link es: ${pokemon?.sprites?.front_default}`)
-  }, [pokemon])
-
- 
-
   return (
     <div>
       <p>{pokemon?.id}</p>
-      <audio controls src={`audio/${pokemon?.id}.ogg`}>
+
+      <div> <img src={pokemon?.sprites?.front_default}></img>
+        <img src={pokemon?.sprites?.front_shiny}></img></div>
+
+
+      <div><audio controls src={`audio/${pokemon?.id}.ogg`}>
         <source type="audio/ogg" />
-        <p>
-          lol
-        </p>
       </audio>
-      <img src={pokemon?.sprites?.front_default}></img>
-      <img src={pokemon?.sprites?.front_shiny}></img>
-    
+      </div>
+      <div>
       <img src={`tipos/${pokemon?.types?.[0]?.type?.name}.jpg`}>
       </img>{
-         pokemon?.types?.[1]?.type?.name &&
+        pokemon?.types?.[1]?.type?.name &&
         <img src={`tipos/${pokemon?.types?.[1]?.type?.name}.jpg`}>
         </img>
-      }
+      }</div>
     </div>)
+    
 }
